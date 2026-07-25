a415ba214168eaef58aa0a0ecbe4f6a2dda84bdb
jziaisle-arch <jziaisle@gmail.com>
2026-07-25 15:58:43 +0800

    Add files via upload

---
+++ middleware/admin.js
@@ -0,0 +1,14 @@
+function admin(req, res, next) {
+
+    if (req.user.role !== "admin") {
+        return res.status(403).json({
+            success: false,
+            message: "Admin access required."
+        });
+    }
+
+    next();
+
+}
+
+module.exports = admin;
